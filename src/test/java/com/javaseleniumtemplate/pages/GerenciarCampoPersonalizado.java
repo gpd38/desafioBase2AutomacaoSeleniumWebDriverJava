b916aba5e75ffda248f161497d177138c68debcd
Gustavo Dias <gustavodias2@gmail.com>
2020-07-13 15:04:40 -0300

    Criando variaveis dos campos a serem mapeados

---
+++ src/test/java/com/javaseleniumtemplate/pages/GerenciarCampoPersonalizado.java
@@ -0,0 +1,11 @@
+package com.javaseleniumtemplate.pages;
+
+import org.openqa.selenium.By;
+
+import com.javaseleniumtemplate.bases.PageBase;
+
+public class GerenciarCampoPersonalizado extends PageBase {
+	
+	By botaoNovoCampo;
+
+}
