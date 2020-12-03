7ebd008dcde37a96c0eaf34f5f66ef52b4d6e6e9
Gustavo Dias <gustavodias2@gmail.com>
2020-12-03 00:30:57 -0300

    Implementando classe ViewIssues e alguns de seus testes

--- codigo/src/test/java/com/javaseleniumtemplate/tests/ViewIssuesTests.java
+++ codigo/src/test/java/com/javaseleniumtemplate/tests/ViewIssuesTests.java
@@ -1,16 +1,59 @@
 package com.javaseleniumtemplate.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import com.javaseleniumtemplate.bases.TestBase;
-import com.javaseleniumtemplate.pages.LoginPage;
+import com.javaseleniumtemplate.flows.LoginFlows;
 import com.javaseleniumtemplate.pages.MainPage;
+import com.javaseleniumtemplate.pages.ViewIssuesPage;
 
 public class ViewIssuesTests extends TestBase {
-
-	LoginPage loginPage;
+	// Objects
+	LoginFlows loginFlows;
 	MainPage mainPage;
+	ViewIssuesPage viewIssuePage;
 
+	// Tests
+	@Test
+	public void acessarViewIssuesComSucesso() {
+		// Objects instances
+		loginFlows = new LoginFlows();
+		mainPage = new MainPage();
+		viewIssuePage = new ViewIssuesPage();
+
+		// Parameteres
+		String usuario = "administrator";
+		String senha = "mantisbt";
+		String mensagemEncontrouPagina = "Viewing Issues";
+
+		// Test
+		loginFlows.efetuarLogin(usuario, senha);
+		mainPage.clickViewIssues();
+
+		//TODO Refatorar este assert
+		Assert.assertEquals(mensagemEncontrouPagina, viewIssuePage.returnPageViewIssueInformation().substring(0, 14));
+	}
+	
+	@Test
+	public void validarURLPage() {
+		// Objects instances
+		loginFlows = new LoginFlows();
+		mainPage = new MainPage();
+		viewIssuePage = new ViewIssuesPage();
+
+		// Parameteres
+		String usuario = "administrator";
+		String senha = "mantisbt";
+		String urlPage = "http://192.168.99.100:8989/view_all_bug_page.php";
+
+		// Test
+		loginFlows.efetuarLogin(usuario, senha);
+		mainPage.clickViewIssues();
+
+		Assert.assertEquals(urlPage, viewIssuePage.returnPageViewIssuesURL());
+	}
+	
 	//@Test
 	public void apagarMarcador() {
 		Assert.fail("Not implemented");
