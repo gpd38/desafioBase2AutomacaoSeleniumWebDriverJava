84855c37d3198885afc48695a9b63bc588c024e3
Gustavo Dias <gustavodias2@gmail.com>
2020-12-03 00:29:24 -0300

    Implementando classe Main e alguns de seus testes

--- codigo/src/test/java/com/javaseleniumtemplate/tests/MainTests.java
+++ codigo/src/test/java/com/javaseleniumtemplate/tests/MainTests.java
@@ -1,24 +1,45 @@
 package com.javaseleniumtemplate.tests;
 
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import com.javaseleniumtemplate.bases.TestBase;
 import com.javaseleniumtemplate.pages.LoginPage;
 import com.javaseleniumtemplate.pages.MainPage;
 
 public class MainTests extends TestBase {
-
+	// Objects
 	LoginPage loginPage;
 	MainPage mainPage;
-
+	
+	// Tests
 	//@Test
 	public void existeTarefaRelatadaPorMim() {
 		Assert.fail("Not implemented");
 	}
 
-	//@Test
+	// @Test
 	public void existeBotaoVerTarefas() {
 		Assert.fail("Not implemented");
+	}
+
+	@Test
+	public void perdeuSuaSenha() {
+		// Objects instances
+		loginPage = new LoginPage();
+		mainPage = new MainPage();
+
+		// Parameteres
+		String usuario = "administrator";
+		String mensagemEncontrouPagina = "Password Reset";
+
+		// Test
+		loginPage.preenhcerUsuario(usuario);
+		loginPage.clicarEmLogin();
+		loginPage.clicarEmPerdeuSenha();
+
+		Assert.assertEquals(mensagemEncontrouPagina, loginPage.returnPagePerdeuSenhaInformation());
+
 	}
 
 }
